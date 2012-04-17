d0b1e4e0da35da6364d6ce650eb79f85eab62dd8
Gary Lai <garylaiph@gmail.com>
2012-04-17 23:16:01 +0800

    product main page

--- javascripts/products.js
+++ javascripts/products.js
@@ -1,5 +1,5 @@
 (function(){
-  $("#products .products_list_selecters").live("click", function(e){
+  $("#products .products_list_selecters, #product_content .product_item").live("click", function(e){
     console.log(".products_list_selecters");
     console.log($(e.currentTarget).attr("target"));
     $("#product_content").load("contents/" + $(e.currentTarget).attr("target"));
