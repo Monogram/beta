0553f093f32e418447ffcf54ce112e0d96a8f385
Gary Lai <garylaiph@gmail.com>
2012-04-17 22:52:01 +0800

    added product

--- javascripts/products.js
+++ javascripts/products.js
@@ -1,5 +1,7 @@
 (function(){
-  $("#products .products img").live("click", function(e){
-    $("#content").load($(e.currentTarget).attr("target"));
+  $("#products .products_list_selecters").live("click", function(e){
+    console.log(".products_list_selecters");
+    console.log($(e.currentTarget).attr("target"));
+    $("#product_content").load("contents/" + $(e.currentTarget).attr("target"));
   });
 })();
